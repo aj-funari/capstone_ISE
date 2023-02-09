9475489d6d53ab0fabf3ad6dc9adc28ed5edfb8a
Ben Bamidele <bbamidel@iu.edu>
2023-02-09 18:21:55 -0500

    Progress

--- ForceTransducers/ForceTransducers.ino
+++ ForceTransducers/ForceTransducers.ino
@@ -1,3 +1,44 @@
+#include <Wire.h>
+
+#include <MadgwickAHRS.h>
+#include <Arduino_LSM9DS1.h>
+#include <Math.h>
+
+/*float pitchFilteredOld;
+Madgwick filter;
+const float sensorRate = 104.00;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if(!IMU.begin())  {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  filter.begin(sensorRate);
+  Serial.println("Setup complete!");
+}  
+
+void loop() {
+  float xAcc, yAcc, zAcc;
+  float xGyro, yGyro, zGyro;
+  float pitch;
+  if(IMU.accelerationAvailable() && IMU.gyroscopeAvailable()){
+    IMU.readAcceleration(xAcc, yAcc, zAcc);
+    IMU.readGyroscope(xGyro, yGyro, zGyro); 
+    filter.updateIMU(xGyro, yGyro, zGyro, xAcc, yAcc, zAcc);
+    //Serial.println("depend");
+    pitch = filter.getPitch(); 
+    Serial.println(pitch);
+    //float pitchFiltered = 0.1 * pitch + 0.9 * pitchFilteredOld; // low pass filter
+    //Serial.println("pitch: " + String(pitchFiltered));
+    //pitchFilteredOld = pitchFiltered;
+  }
+}*/
+
+
 int val_pot;
 void setup() {
   // put your setup code here, to run once:
@@ -10,7 +51,7 @@ void loop() {
   val_pot = analogRead(A1);
   Serial.print("The digitial value is: ");
   if (val_pot > 200){
-    digitalWrite(22,LOW);
+    digitalWrite(LED,LOW);
   }
   else {
     digitalWrite(22,HIGH);
